13255b4379f27c20a7341e95b56c868ff72b0558
Nicholas Biddell <nicbiddell@gmail.com>
2017-05-22 22:12:28 -0400

    Create initial app migrations and tests for seasons

--- config/settings/base.py
+++ config/settings/base.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'sc2league-server.seasons'
+    'sc2league-server.seasons',
 ]
 
 MIDDLEWARE = [
