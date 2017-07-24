92ba380b681894186d693d96fbacdb4b44cc0413
Nicholas Biddell <nicbiddell@gmail.com>
2017-07-23 21:26:00 -0400

    Run server through gunicorn instead of Django dev server

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,9 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --clear --no-input
 
+exec gunicorn config.wsgi:application \
+    --name sc2league-server \
+    --bind 0.0.0.0:8000 \
+    --workers 3 \
+
 exec "$@"
