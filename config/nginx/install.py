29740aae679a4a8d1933b476f180e7839e942ba6
Nicholas Biddell <nicbiddell@gmail.com>
2017-08-26 01:16:24 -0400

    Run different installs based on env var

--- config/nginx/install.py
+++ config/nginx/install.py
@@ -1,14 +1,17 @@
 import os
-
 import subprocess
+
+from jinja2 import Template
+
+from utilities import copy, template
 
 def get_ssl_context(environ):
     domain = environ['DOMAIN']
     nginx_info = {
-        'domain': domain,
-        'dhparam': False,
-        'ssl_trusted_certificate': False
-    }
+            'domain': domain,
+            'dhparam': False,
+            'ssl_trusted_certificate': False
+            }
 
     dhparam = os.path.join('/certificates', domain + '.dhparam.pem')
     if os.path.exists(dhparam):
@@ -27,22 +30,39 @@ def get_certificates(domain):
     certificate = os.path.join('/certificates', domain + '.crt')
 
     if not (os.path.exists(private_key) or os.path.exists(certificate)):
-        cmd = """openssl req \
-                - new \
-                - newkey rsa:4096 \
-                - days 365 \
-                - nodes \
-                - x509 \
-                - subj "/C=US/ST=State/L=City/O=Company/CN={}" \
-                - keyout {} \ 
-                - out {}""".format(domain, private_key, certificate)
+        cmd = """openssl req \ - new \
+        - newkey rsa:4096 \
+        - days 365 \
+        - nodes \
+        - x509 \
+        - subj "/C=US/ST=State/L=City/O=Company/CN={}" \
+        - keyout {} \
+        - out {}""".format(domain, private_key, certificate)
+        
         subprocess.call(cmd, shell=True)
 
     return private_key, certificate
 
-if __name__ == "__main__":
+def local_server():
     pk, crt = get_certificates(os.environ['DOMAIN'])
     copy(source=pk, destination='/etc/ssl/private', basename='private.key', mode='0600')
     copy(source=crt, destination='/etc/ssl/certs', basename='certificate.crt', mode='0600')
 
     template('/api.conf.j2', get_ssl_context(os.environ), '/etc/nginx/conf.d/default.conf')
+
+def lets_encrypt_server():
+    context = {
+            'domain': os.environ['DOMAIN']
+            }
+
+    jinja_template = Template(open('/api.conf.j2').read())
+    with open('/etc/nginx/conf.d/default.conf', 'w') as f:
+        f.write(jinja_template.render(context))
+
+if __name__ == "__main__":
+    use_letsencrypt = os.environ.get("DOCKER_NGINX_LETSENCRYPT")
+
+    if use_letsencrypt is not None and use_letsencrypt == 'T':
+        lets_encrypt_server()
+    else:
+        local_server()
